d22b72b65097e596e95b7a5f0b8cf3d31850f0d8
Bryan Gorges <b.gorges@jach.hawaii.edu>
2014-07-28 15:09:46 -1000

    Initial tle commit.

---
+++ lib/omp/tle/spaceTracker.py
@@ -0,0 +1,53 @@
+from requests import session
+import sys
+
+def helpUser():
+    '''Prints help directions'''
+    print '''spaceTracker.py -- For use by JAC
+********************************************
+Please enter a list of space separated satellite numbers or
+a path to a text file.'''
+    return
+
+def parse(nums_in): #not going to work. Will take one satellite the wrong way.
+    list_nums = []
+    if type(nums_in) is list:
+        #set list to list
+        list_nums = nums_in
+    if type(nums_in) is str:
+        #open file and get list into list_nums
+        pass
+    #check list_nums to see that they conform to the NORAD_CAT_ID
+
+#if len(sys.argv) < 2:
+#    helpUser()
+#    quit()
+
+url = "https://www.space-track.org/ajaxauth/login"
+user = "ukirtot@jach.hawaii.edu"
+password = ""
+idpass = {'identity': user, 'password': password}
+
+with session() as ss:
+    r = ss.post(url, data=idpass)
+    print r.text
+    rurl = "https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/25544/orderby/EPOCH desc/limit/22/format/tle"
+    r = ss.get(rurl)
+    print r.text
+
+class SpaceTrack(object):
+    """space-track.org API"""
+    def __init__(self):
+        pass
+
+    def add_id(self, catid):
+        """Take NORAD Cat ID and adds it to the list."""
+        pass
+
+    def build_requests(self):
+        """Lumps the IDs that can be lumped and creates a batch of requests."""
+        pass
+
+    def send_requests(self):
+        """Sends current batch of requests."""
+        pass
---
+++ lib/omp/tle/test-tle.py
@@ -0,0 +1,3 @@
+#test-tle.py
+
+import spaceTracker

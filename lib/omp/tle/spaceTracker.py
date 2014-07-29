c650ce2adec70d5eb6383a4f8a854bdd8321d73d
Bryan Gorges <b.gorges@jach.hawaii.edu>
2014-07-28 16:37:24 -1000

    Start building SpaceTrack add_id with unittests.
    Comment out code that needs to go in classes.

--- lib/omp/tle/spaceTracker.py
+++ lib/omp/tle/spaceTracker.py
@@ -1,48 +1,59 @@
-from requests import session
-import sys
+# from requests import session
+# import sys
 
-def helpUser():
-    '''Prints help directions'''
-    print '''spaceTracker.py -- For use by JAC
-********************************************
-Please enter a list of space separated satellite numbers or
-a path to a text file.'''
-    return
+# def helpUser():
+#     '''Prints help directions'''
+#     print '''spaceTracker.py -- For use by JAC
+# ********************************************
+# Please enter a list of space separated satellite numbers or
+# a path to a text file.'''
+#     return
 
-def parse(nums_in): #not going to work. Will take one satellite the wrong way.
-    list_nums = []
-    if type(nums_in) is list:
-        #set list to list
-        list_nums = nums_in
-    if type(nums_in) is str:
-        #open file and get list into list_nums
-        pass
-    #check list_nums to see that they conform to the NORAD_CAT_ID
+# def parse(nums_in): #not going to work. Will take one satellite the wrong way.
+#     list_nums = []
+#     if type(nums_in) is list:
+#         #set list to list
+#         list_nums = nums_in
+#     if type(nums_in) is str:
+#         #open file and get list into list_nums
+#         pass
+#     #check list_nums to see that they conform to the NORAD_CAT_ID
 
-#if len(sys.argv) < 2:
-#    helpUser()
-#    quit()
+# #if len(sys.argv) < 2:
+# #    helpUser()
+# #    quit()
 
-url = "https://www.space-track.org/ajaxauth/login"
-user = "ukirtot@jach.hawaii.edu"
-password = ""
-idpass = {'identity': user, 'password': password}
+# url = "https://www.space-track.org/ajaxauth/login"
+# user = "ukirtot@jach.hawaii.edu"
+# password = ""
+# idpass = {'identity': user, 'password': password}
 
-with session() as ss:
-    r = ss.post(url, data=idpass)
-    print r.text
-    rurl = "https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/25544/orderby/EPOCH desc/limit/22/format/tle"
-    r = ss.get(rurl)
-    print r.text
+# with session() as ss:
+#     r = ss.post(url, data=idpass)
+#     print r.text
+#     rurl = "https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/25544/orderby/EPOCH desc/limit/22/format/tle"
+#     r = ss.get(rurl)
+#     print r.text
 
 class SpaceTrack(object):
     """space-track.org API"""
     def __init__(self):
-        pass
+        self.id_list = []
+        self.req_list = []
 
     def add_id(self, catid):
         """Take NORAD Cat ID and adds it to the list."""
-        pass
+        if len(catid) > 5:
+            try:
+                raise ValueError("catid requires 5 or fewer digits.")
+            except ValueError:
+                print "Not good."
+                raise
+            return
+        elif len(catid) < 5:
+            temp = (5 - len(catid)) * '0'
+            catid = temp + catid
+        self.id_list.append(catid)
 
     def build_requests(self):
         """Lumps the IDs that can be lumped and creates a batch of requests."""
--- lib/omp/tle/test-tle.py
+++ lib/omp/tle/test-tle.py
@@ -1,3 +1,39 @@
 #test-tle.py
-
+import unittest
+import random
 import spaceTracker
+
+
+class TestSpaceTrack(unittest.TestCase):
+
+	def setUp(self):
+		self.st = spaceTracker.SpaceTrack()
+
+	def tearDown(self):
+		self.st = None
+
+	def test_add_id_exception(self):
+		"""
+		Any integer more than 5 digits should raise an exception
+		"""
+		self.assertRaises(ValueError, self.st.add_id, '255678')
+
+	def test_add_id_short(self):
+		"""Test random numbers under 5 digits."""
+		for i in range(1000):
+			rnd = random.randint(1,9999)
+			rnd_str = str(rnd)
+			self.st.add_id(rnd_str)
+			self.assertEqual(int(self.st.id_list[-1]), rnd)
+
+	def test_add_id_five(self):
+		"""Test random five digit numbers"""
+		for i in range(1000):
+			rnd = random.randint(10000,99999)
+			rnd_str = str(rnd)
+			self.st.add_id(rnd_str)
+			self.assertEqual(int(self.st.id_list[-1]), rnd)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSpaceTrack)
+unittest.TextTestRunner(verbosity=3).run(suite)

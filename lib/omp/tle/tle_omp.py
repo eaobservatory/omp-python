87775aaeeb9f45a6a48f3c480a4b51f0a2eea10f
Graham Bell <g.bell@jach.hawaii.edu>
2014-08-22 10:59:55 -1000

    TLE: use separate cursors in the database methods

--- lib/omp/tle/tle_omp.py
+++ lib/omp/tle/tle_omp.py
@@ -1,7 +1,9 @@
 #tle_omp.py
 
+from contextlib import closing
 import logging
 import Sybase
+
 import omp.siteconfig as siteconfig
 
 logger = logging.getLogger(__name__)
@@ -16,63 +18,70 @@ class TLE_OMP(object):
 		logger.debug('Connecting to OMP, user:%s database:%s',
 		             user, omp)
 		self.db = Sybase.connect('SYB_JAC', user, password, omp)
-		self.cursor = self.db.cursor()
 
 	def submit_tle(self, tle):
 		"""Takes tle and submits it into omp db"""
-		logger.debug('Deleting old omptle row for "%s"', tle['target'])
-		self.cursor.execute("DELETE FROM omptle WHERE target=@target",
+		with closing(self.db.cursor()) as cursor:
+			logger.debug('Deleting old omptle row for "%s"', tle['target'])
+			cursor.execute("DELETE FROM omptle WHERE target=@target",
+								{
+								'@target':tle["target"]
+								}
+							)
+
+			logger.debug('Inserting new omptle row: %s', repr(tle))
+			cursor.execute("""
+					INSERT INTO omptle
+					(target, el1, el2, el3, el4, el5, el6, el7, el8)
+					VALUES
+					(@target, @el1, @el2, @el3, @el4, @el5, @el6, @el7, @el8)
+							""",
 							{
-							 '@target':tle["target"]
-							}
-						   )
-		self.cursor.execute("""
-				INSERT INTO omptle
-				(target, el1, el2, el3, el4, el5, el6, el7, el8)
-				VALUES
-				(@target, @el1, @el2, @el3, @el4, @el5, @el6, @el7, @el8)
-						   """,
-						   {
-						   	'@target': tle["target"],
-						   	'@el1': tle["el1"],
-						   	'@el2': tle["el2"],
-						   	'@el3': tle["el3"],
-						   	'@el4': tle["el4"],
-						   	'@el5': tle["el5"],
-						   	'@el6': tle["el6"],
-						   	'@el7': tle["el7"],
-						   	'@el8': tle["el8"]
-						   })
+								'@target': tle["target"],
+								'@el1': tle["el1"],
+								'@el2': tle["el2"],
+								'@el3': tle["el3"],
+								'@el4': tle["el4"],
+								'@el5': tle["el5"],
+								'@el6': tle["el6"],
+								'@el7': tle["el7"],
+								'@el8': tle["el8"]
+							})
 
 		logger.debug('Committing transaction')
 		self.db.commit()
 
 	def retrieve_ids(self):
 		"""Finds all auto update tles"""
-		logger.debug('Retrieving list of distinct AUTO-TLE targets from ompobs')
-		self.cursor.execute("SELECT DISTINCT target FROM ompobs WHERE coordstype=\"AUTO-TLE\"")
-		return [r[0] for r in self.cursor.fetchall()]
+		with closing(self.db.cursor()) as cursor:
+			logger.debug('Retrieving list of distinct AUTO-TLE targets from ompobs')
+			cursor.execute("SELECT DISTINCT target FROM ompobs WHERE coordstype=\"AUTO-TLE\"")
+			rows = cursor.fetchall()
+
+		return [r[0] for r in rows]
 
 	def update_tle_ompobs(self, tle):
 		"""Places elements in omp."""
-		logger.debug('Updating ompobs with: %s', repr(tle))
-		self.cursor.execute("""
-				UPDATE ompobs SET
-				el1=@el1, el2=@el2, el3=@el3, el4=@el4,
-				el5=@el5, el6=@el6, el7=@el7, el8=@el8
-				WHERE coordstype="AUTO-TLE" AND target=@target
-						   """,
-						   {
-						   	'@el1': tle["el1"],
-						   	'@el2': tle["el2"],
-						   	'@el3': tle["el3"],
-						   	'@el4': tle["el4"],
-						   	'@el5': tle["el5"],
-						   	'@el6': tle["el6"],
-						   	'@el7': tle["el7"],
-						   	'@el8': tle["el8"],
-						   	'@target': tle["target"]
-						   })
+
+		with closing(self.db.cursor()) as cursor:
+			logger.debug('Updating ompobs with: %s', repr(tle))
+			cursor.execute("""
+					UPDATE ompobs SET
+					el1=@el1, el2=@el2, el3=@el3, el4=@el4,
+					el5=@el5, el6=@el6, el7=@el7, el8=@el8
+					WHERE coordstype="AUTO-TLE" AND target=@target
+							""",
+							{
+								'@el1': tle["el1"],
+								'@el2': tle["el2"],
+								'@el3': tle["el3"],
+								'@el4': tle["el4"],
+								'@el5': tle["el5"],
+								'@el6': tle["el6"],
+								'@el7': tle["el7"],
+								'@el8': tle["el8"],
+								'@target': tle["target"]
+							})
 
 		logger.debug('Committing transaction')
 		self.db.commit()

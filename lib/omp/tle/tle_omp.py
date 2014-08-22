82532aa4116641c8cecb60ecd90e35f01d00676e
Graham Bell <g.bell@jach.hawaii.edu>
2014-08-22 10:25:19 -1000

    TLE: add logging to OMP TLE module

--- lib/omp/tle/tle_omp.py
+++ lib/omp/tle/tle_omp.py
@@ -1,7 +1,11 @@
 #tle_omp.py
 
+import logging
 import Sybase
 import omp.siteconfig as siteconfig
+
+logger = logging.getLogger(__name__)
+
 
 class TLE_OMP(object):
 	"""Opens connection to omp database and allows tles to be submitted.
@@ -9,11 +13,14 @@ class TLE_OMP(object):
 	"""
 	def __init__(self, omp="devomp"):
 		user, password = self.enter_omp()
+		logger.debug('Connecting to OMP, user:%s database:%s',
+		             user, omp)
 		self.db = Sybase.connect('SYB_JAC', user, password, omp)
 		self.cursor = self.db.cursor()
 
 	def submit_tle(self, tle):
 		"""Takes tle and submits it into omp db"""
+		logger.debug('Deleting old omptle row for "%s"', tle['target'])
 		self.cursor.execute("DELETE FROM omptle WHERE target=@target",
 							{
 							 '@target':tle["target"]
@@ -36,15 +43,19 @@ class TLE_OMP(object):
 						   	'@el7': tle["el7"],
 						   	'@el8': tle["el8"]
 						   })
+
+		logger.debug('Committing transaction')
 		self.db.commit()
 
 	def retrieve_ids(self):
 		"""Finds all auto update tles"""
+		logger.debug('Retrieving list of distinct AUTO-TLE targets from ompobs')
 		self.cursor.execute("SELECT DISTINCT target FROM ompobs WHERE coordstype=\"AUTO-TLE\"")
 		return [r[0] for r in self.cursor.fetchall()]
 
 	def update_tle_ompobs(self, tle):
 		"""Places elements in omp."""
+		logger.debug('Updating ompobs with: %s', repr(tle))
 		self.cursor.execute("""
 				UPDATE ompobs SET
 				el1=@el1, el2=@el2, el3=@el3, el4=@el4,
@@ -62,6 +73,8 @@ class TLE_OMP(object):
 						   	'@el8': tle["el8"],
 						   	'@target': tle["target"]
 						   })
+
+		logger.debug('Committing transaction')
 		self.db.commit()
 
 	def enter_omp(self):

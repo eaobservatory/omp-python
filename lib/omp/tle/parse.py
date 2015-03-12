14069dec631f8a6cde4d70516d21fd8b6a95ff81
Graham Bell <g.bell@eaobservatory.org>
2015-03-11 14:50:08 -1000

    TLE: adjust indentation (mostly removing tabs)
    Also replace the do-nothing test "self.assertEqual(1,1)" with "pass"
    since the former isn't PEP-8 compliant.

--- lib/omp/tle/parse.py
+++ lib/omp/tle/parse.py
@@ -16,102 +16,102 @@
 from codecs import latin_1_encode
 import datetime
 import logging
-#from degrees to radians
 from math import radians
 
 logger = logging.getLogger(__name__)
 
+
 class TLEParser(object):
-	"""TLEParser"""
-	def __init__(self, tletype="NORAD"):
-		self.tletype = tletype
-		self.tle = {tletype: "",
-					"Class": "",
-					"Intl Desig": "",
-					"Epoch": "",
-					"First D": "",
-					"Second D": "",
-					"Bstar": "",
-					"ElSet Type": "",
-					"Element Num": "",
-					"Inclination": "",
-					"RA A Node": "",
-					"E": "",
-					"Perigee": "",
-					"Mean Anomoly": "",
-					"Mean Motion": "",
-					"Rev at Epoch": ""}
+    """TLEParser"""
+    def __init__(self, tletype="NORAD"):
+        self.tletype = tletype
+        self.tle = {tletype: "",
+                    "Class": "",
+                    "Intl Desig": "",
+                    "Epoch": "",
+                    "First D": "",
+                    "Second D": "",
+                    "Bstar": "",
+                    "ElSet Type": "",
+                    "Element Num": "",
+                    "Inclination": "",
+                    "RA A Node": "",
+                    "E": "",
+                    "Perigee": "",
+                    "Mean Anomoly": "",
+                    "Mean Motion": "",
+                    "Rev at Epoch": ""}
 
-	def convert_epoch(self, astro):
-		astro = astro.strip()
-		year = "20" + astro[:2]
-		day = astro[2:astro.find(".")]
-		tday = datetime.datetime.strptime(year + " " + day, '%Y %j')
-		eday = datetime.datetime.strptime("1970 1", '%Y %j')
-		days = (tday - eday).days
-		return (float(astro[astro.find("."):]) + days) * 24 * 3600
+    def convert_epoch(self, astro):
+        astro = astro.strip()
+        year = "20" + astro[:2]
+        day = astro[2:astro.find(".")]
+        tday = datetime.datetime.strptime(year + " " + day, '%Y %j')
+        eday = datetime.datetime.strptime("1970 1", '%Y %j')
+        days = (tday - eday).days
+        return (float(astro[astro.find("."):]) + days) * 24 * 3600
 
-	def export_tle_omp(self, tle):
-		elements = {}
-		elements["target"] = tle["NORAD"] #self.tletype]
-		elements["el1"] = tle["Epoch"]
-		elements["el2"] = tle["Bstar"]
-		elements["el3"] = tle["Inclination"]
-		elements["el4"] = tle["RA A Node"]
-		elements["el5"] = tle["E"]
-		elements["el6"] = tle["Perigee"]
-		elements["el7"] = tle["Mean Anomoly"]
-		elements["el8"] = tle["Mean Motion"]
-		return elements
+    def export_tle_omp(self, tle):
+        elements = {}
+        elements["target"] = tle["NORAD"]  # self.tletype]
+        elements["el1"] = tle["Epoch"]
+        elements["el2"] = tle["Bstar"]
+        elements["el3"] = tle["Inclination"]
+        elements["el4"] = tle["RA A Node"]
+        elements["el5"] = tle["E"]
+        elements["el6"] = tle["Perigee"]
+        elements["el7"] = tle["Mean Anomoly"]
+        elements["el8"] = tle["Mean Motion"]
+        return elements
 
-	def write_tle(self, tle, ofile=None, obuffer=None):
-		if ofile is not None:
-			pass
-		elif obuffer is not None:
-			pass
-		else:
-			#raise exception
-			return
+    def write_tle(self, tle, ofile=None, obuffer=None):
+        if ofile is not None:
+            pass
+        elif obuffer is not None:
+            pass
+        else:
+            # raise exception
+            return
 
-	def parse_tle(self, line1, line2):
-		logger.debug('Parsing TLE, line 1: {0}'.format(line1))
-		logger.debug('Parsing TLE, line 2: {0}'.format(line2))
+    def parse_tle(self, line1, line2):
+        logger.debug('Parsing TLE, line 1: {0}'.format(line1))
+        logger.debug('Parsing TLE, line 2: {0}'.format(line2))
 
-		if len(line1) < 62 or len(line2) < 69:
-			return None
-		tle = self.tle.copy()
-		tle[self.tletype] = self.tletype + latin_1_encode(line1[2:7])[0]
-		tle["Class"] = line1[7]
-		tle["Intl Desig"] = line1[9:17]
-		tle["Epoch"] = self.convert_epoch(line1[18:32])
-		tle["First D"] = line1[33:43]
-		tle["Second D"] = line1[44:52]
-		tle["Bstar"] = self._parse_decimal_rhs(line1[53:61])
-		tle["ElSet Type"] = line1[62]
-		tle["Element Num"] = line1[64:68]
-		tle["Inclination"] = radians(float(line2[8:16]))
-		tle["RA A Node"] = radians(float(line2[17:25]))
-		tle["E"] = float("." + line2[26:33])
-		tle["Perigee"] = radians(float(line2[34:42]))
-		tle["Mean Anomoly"] = radians(float(line2[43:51]))
-		tle["Mean Motion"] = float(line2[52:63])
-		tle["Rev at Epoch"] = line2[63:68]
-		return tle
+        if len(line1) < 62 or len(line2) < 69:
+            return None
+        tle = self.tle.copy()
+        tle[self.tletype] = self.tletype + latin_1_encode(line1[2:7])[0]
+        tle["Class"] = line1[7]
+        tle["Intl Desig"] = line1[9:17]
+        tle["Epoch"] = self.convert_epoch(line1[18:32])
+        tle["First D"] = line1[33:43]
+        tle["Second D"] = line1[44:52]
+        tle["Bstar"] = self._parse_decimal_rhs(line1[53:61])
+        tle["ElSet Type"] = line1[62]
+        tle["Element Num"] = line1[64:68]
+        tle["Inclination"] = radians(float(line2[8:16]))
+        tle["RA A Node"] = radians(float(line2[17:25]))
+        tle["E"] = float("." + line2[26:33])
+        tle["Perigee"] = radians(float(line2[34:42]))
+        tle["Mean Anomoly"] = radians(float(line2[43:51]))
+        tle["Mean Motion"] = float(line2[52:63])
+        tle["Rev at Epoch"] = line2[63:68]
+        return tle
 
-	def _parse_decimal_rhs(self, decimal):
-		"""Routine to parse TLE-style right hand sides of
-		truncated decimals.  (i.e. the bit after the decimal
-		point)
-		"""
-		if decimal.startswith('-'):
-			decimal = decimal[1:]
-			sign = -1.0
-		else:
-			sign = 1.0
+    def _parse_decimal_rhs(self, decimal):
+        """Routine to parse TLE-style right hand sides of
+        truncated decimals.  (i.e. the bit after the decimal
+        point)
+        """
+        if decimal.startswith('-'):
+            decimal = decimal[1:]
+            sign = -1.0
+        else:
+            sign = 1.0
 
-		if '-' in decimal:
-			decimal = 'E-'.join(decimal.split('-'))
-		elif '+' in decimal:
-			decimal = 'E+'.join(decimal.split('+'))
+        if '-' in decimal:
+            decimal = 'E-'.join(decimal.split('-'))
+        elif '+' in decimal:
+            decimal = 'E+'.join(decimal.split('+'))
 
-		return sign * float('0.' + decimal.strip())
+        return sign * float('0.' + decimal.strip())
--- test/test_tle.py
+++ test/test_tle.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#test-tle.py
+
 import unittest
 import random
 import time
@@ -22,121 +22,121 @@ from omp.db.part.tle import TLEDB
 
 
 class TestSpaceTrack(unittest.TestCase):
-	def setUp(self):
-		self.st = SpaceTrack()
+    def setUp(self):
+        self.st = SpaceTrack()
 
-	def tearDown(self):
-		self.st = None
+    def tearDown(self):
+        self.st = None
 
-	def test_add_id_exception(self):
-		"""
-		Any integer more than 5 digits should raise an exception
-		"""
-		self.assertRaises(ValueError, self.st.add_id, '255678')
+    def test_add_id_exception(self):
+        """
+        Any integer more than 5 digits should raise an exception
+        """
+        self.assertRaises(ValueError, self.st.add_id, '255678')
 
-	def test_add_id_short(self):
-		"""Test random numbers under 5 digits."""
-		for i in range(1000):
-			rnd = random.randint(1,9999)
-			rnd_str = str(rnd)
-			self.st.add_id(rnd_str)
-			self.assertEqual(int(self.st.id_list[-1]), rnd)
+    def test_add_id_short(self):
+        """Test random numbers under 5 digits."""
+        for i in range(1000):
+            rnd = random.randint(1, 9999)
+            rnd_str = str(rnd)
+            self.st.add_id(rnd_str)
+            self.assertEqual(int(self.st.id_list[-1]), rnd)
 
-	def test_add_id_five(self):
-		"""Test random five digit numbers"""
-		for i in range(1000):
-			rnd = random.randint(10000,99999)
-			rnd_str = str(rnd)
-			self.st.add_id(rnd_str)
-			self.assertEqual(int(self.st.id_list[-1]), rnd)
+    def test_add_id_five(self):
+        """Test random five digit numbers"""
+        for i in range(1000):
+            rnd = random.randint(10000, 99999)
+            rnd_str = str(rnd)
+            self.st.add_id(rnd_str)
+            self.assertEqual(int(self.st.id_list[-1]), rnd)
 
-	def test_add_id_numeral(self):
-		"""Test random integers"""
-		for i in range(1000):
-			rnd = random.randint(1,99999)
-			self.st.add_id(rnd)
-			self.assertEqual(int(self.st.id_list[-1]), rnd)
+    def test_add_id_numeral(self):
+        """Test random integers"""
+        for i in range(1000):
+            rnd = random.randint(1, 99999)
+            self.st.add_id(rnd)
+            self.assertEqual(int(self.st.id_list[-1]), rnd)
 
-	def test_build_request(self):
-		"""Too simple? build request test."""
-		_list = [345, 20, 45034, 2401, 4242]
-		for each in _list:
-			self.st.add_id(each)
-		_list.sort()
-		comma_str = ",".join([str(r) for r in _list])
-		self.assertEqual(
-			self.st._build_request(self.st.id_list),
-			"https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/NORAD_CAT_ID/20,345,2401,4242,45034/orderby/EPOCH desc/format/tle")
+    def test_build_request(self):
+        """Too simple? build request test."""
+        _list = [345, 20, 45034, 2401, 4242]
+        for each in _list:
+            self.st.add_id(each)
+        _list.sort()
+        comma_str = ",".join([str(r) for r in _list])
+        self.assertEqual(
+            self.st._build_request(self.st.id_list),
+            "https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/NORAD_CAT_ID/20,345,2401,4242,45034/orderby/EPOCH desc/format/tle")
 
 
 class TestTLEParse(unittest.TestCase):
-	def setUp(self):
-		self.parse = TLEParser()
+    def setUp(self):
+        self.parse = TLEParser()
 
-	def tearDown(self):
-		self.parse = None
+    def tearDown(self):
+        self.parse = None
 
-	def test_parse_decimal_rhs(self):
-		self.assertEqual(self.parse._parse_decimal_rhs(' 00000+0'), 0.0)
-		self.assertEqual(self.parse._parse_decimal_rhs(' 00000-0'), 0.0)
-		self.assertEqual(self.parse._parse_decimal_rhs(' 10000+0'), 0.1)
-		self.assertEqual(self.parse._parse_decimal_rhs(' 10000-0'), 0.1)
-		self.assertEqual(self.parse._parse_decimal_rhs(' 10000+1'), 1.0)
-		self.assertEqual(self.parse._parse_decimal_rhs(' 10000-1'), 0.01)
-		self.assertEqual(self.parse._parse_decimal_rhs('-00000+0'), -0.0)
-		self.assertEqual(self.parse._parse_decimal_rhs('-00000-0'), -0.0)
-		self.assertEqual(self.parse._parse_decimal_rhs('-10000+0'), -0.1)
-		self.assertEqual(self.parse._parse_decimal_rhs('-10000-0'), -0.1)
-		self.assertEqual(self.parse._parse_decimal_rhs('-10000+1'), -1.0)
-		self.assertEqual(self.parse._parse_decimal_rhs('-10000-1'), -0.01)
+    def test_parse_decimal_rhs(self):
+        self.assertEqual(self.parse._parse_decimal_rhs(' 00000+0'), 0.0)
+        self.assertEqual(self.parse._parse_decimal_rhs(' 00000-0'), 0.0)
+        self.assertEqual(self.parse._parse_decimal_rhs(' 10000+0'), 0.1)
+        self.assertEqual(self.parse._parse_decimal_rhs(' 10000-0'), 0.1)
+        self.assertEqual(self.parse._parse_decimal_rhs(' 10000+1'), 1.0)
+        self.assertEqual(self.parse._parse_decimal_rhs(' 10000-1'), 0.01)
+        self.assertEqual(self.parse._parse_decimal_rhs('-00000+0'), -0.0)
+        self.assertEqual(self.parse._parse_decimal_rhs('-00000-0'), -0.0)
+        self.assertEqual(self.parse._parse_decimal_rhs('-10000+0'), -0.1)
+        self.assertEqual(self.parse._parse_decimal_rhs('-10000-0'), -0.1)
+        self.assertEqual(self.parse._parse_decimal_rhs('-10000+1'), -1.0)
+        self.assertEqual(self.parse._parse_decimal_rhs('-10000-1'), -0.01)
 
-	def test_parse_tle(self):
-		"""print parse"""
-		ans = self.parse.parse_tle("1 25544U 98067A   14206.52997318 -.00005757  00000-0 -91404-4 0  7690",
-							 "2 25544 051.6472 269.5323 0006361 286.1580 210.2768 15.50427728897273")
-		self.assertEqual(ans, {'E': 0.0006361, 'RA A Node': 4.7042260754731124,
-							   'First D': '-.00005757', 'Epoch': 1406292189.682752,
-							   'Intl Desig': '98067A  ', 'Bstar': -0.000091404,
-							   'Inclination': 0.9014136894360153, 'Mean Motion': 15.50427728,
-							   'NORAD': 'NORAD25544', 'Perigee': 4.994399280921934,
-							   'ElSet Type': '0', 'Element Num': ' 769',
-							   'Second D': ' 00000-0', 'Rev at Epoch': '89727',
-							   'Class': 'U', 'Mean Anomoly': 3.6700225005576126})
+    def test_parse_tle(self):
+        """print parse"""
+        ans = self.parse.parse_tle("1 25544U 98067A   14206.52997318 -.00005757  00000-0 -91404-4 0  7690",
+                                   "2 25544 051.6472 269.5323 0006361 286.1580 210.2768 15.50427728897273")
+        self.assertEqual(ans, {'E': 0.0006361, 'RA A Node': 4.7042260754731124,
+                               'First D': '-.00005757', 'Epoch': 1406292189.682752,
+                               'Intl Desig': '98067A  ', 'Bstar': -0.000091404,
+                               'Inclination': 0.9014136894360153, 'Mean Motion': 15.50427728,
+                               'NORAD': 'NORAD25544', 'Perigee': 4.994399280921934,
+                               'ElSet Type': '0', 'Element Num': ' 769',
+                               'Second D': ' 00000-0', 'Rev at Epoch': '89727',
+                               'Class': 'U', 'Mean Anomoly': 3.6700225005576126})
 
-	def test_convert_epoch(self):
-		"""Test Epoch converter"""
-		epoch = "14099.5"
-		self.assertEqual(1397044800.0, self.parse.convert_epoch(epoch))
+    def test_convert_epoch(self):
+        """Test Epoch converter"""
+        epoch = "14099.5"
+        self.assertEqual(1397044800.0, self.parse.convert_epoch(epoch))
 
-	def test_export_tle_omp(self):
-		ans = self.parse.parse_tle("1 25544U 98067A   14206.52997318 -.00005757  00000-0 -91404-4 0  7690",
-							 "2 25544 051.6472 269.5323 0006361 286.1580 210.2768 15.50427728897273")
-		export = self.parse.export_tle_omp(ans)
-		self.assertEqual(export,{'target': 'NORAD25544', 'el8': 15.50427728,
-								 'el2': -0.000091404, 'el3': 0.9014136894360153,
-								 'el1': 1406292189.682752, 'el6': 4.994399280921934,
-								 'el7': 3.6700225005576126, 'el4': 4.7042260754731124,
-								 'el5': 0.0006361} )
+    def test_export_tle_omp(self):
+        ans = self.parse.parse_tle("1 25544U 98067A   14206.52997318 -.00005757  00000-0 -91404-4 0  7690",
+                                   "2 25544 051.6472 269.5323 0006361 286.1580 210.2768 15.50427728897273")
+        export = self.parse.export_tle_omp(ans)
+        self.assertEqual(export, {'target': 'NORAD25544', 'el8': 15.50427728,
+                                  'el2': -0.000091404, 'el3': 0.9014136894360153,
+                                  'el1': 1406292189.682752, 'el6': 4.994399280921934,
+                                  'el7': 3.6700225005576126, 'el4': 4.7042260754731124,
+                                  'el5': 0.0006361})
 
 
 class TestTLEOMP(unittest.TestCase):
-	def setUp(self):
-		self.subomp = TLEDB(read_only=True)
+    def setUp(self):
+        self.subomp = TLEDB(read_only=True)
 
-	def tearDown(self):
-		self.subomp = None
+    def tearDown(self):
+        self.subomp = None
 
-	def test_submit_tle(self):
-#		self.subomp.submit_tle({'target': 'NORAD25544', 'el8': 15.50427728,
-#								 'el2': -0.000091404, 'el3': 0.9014136894360153,
-#								 'el1': 1406292189.682752, 'el6': 4.994399280921934,
-#								 'el7': 3.6700225005576126, 'el4': 4.7042260754731124,
-#								 'el5': 0.0006361})
-		self.assertEqual(1,1)
+    def test_submit_tle(self):
+        # self.subomp.submit_tle({'target': 'NORAD25544', 'el8': 15.50427728,
+        #                          'el2': -0.000091404, 'el3': 0.9014136894360153,
+        #                          'el1': 1406292189.682752, 'el6': 4.994399280921934,
+        #                          'el7': 3.6700225005576126, 'el4': 4.7042260754731124,
+        #                          'el5': 0.0006361})
+        pass
 
-	def test_update_tle_ompobs(self):
-		self.assertEqual(1,1)
+    def test_update_tle_ompobs(self):
+        pass
 
-	def test_retrieve_ids(self):
-#		self.assertEqual(["NORAD39504"], self.subomp.retrieve_ids())
-                pass
+    def test_retrieve_ids(self):
+        # self.assertEqual(["NORAD39504"], self.subomp.retrieve_ids())
+        pass
